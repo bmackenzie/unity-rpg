31627005473bcf3a7fddcc5a8dd906ad549d3352
bmackenzie <brian.e.mackenzie@outlook.com>
2020-10-11 23:13:32 -0500

    Dialog system started
    Added the ability to talk to NPCs, needs finishing (lock player movement, add nameplate functionality).

---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogManager : MonoBehaviour
+{
+    //variables to store UI objects
+    public Text dialogText;
+    public Text nameText;
+    public GameObject dialogBox;
+    public GameObject nameBox;
+
+    //variable to store dialog by line
+    public string[] dialogLines;
+
+    //variable to store line being displayed
+    public int currentLine;
+
+    //variable checking if dialog just started to make sure the first line isn't skipped
+    bool justStarted;
+
+    //instanced for easy referencing since their will only ever be one
+    public static DialogManager instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //moves through dialog on button press.  On button release instead of press. Checks to see if dialog just started to avoid skipping the first line
+        if (dialogBox.activeInHierarchy)
+        {
+            if (Input.GetButtonUp("Fire1"))
+            {
+                if (!justStarted)
+                {
+                    currentLine++;
+
+                    //changes dialog text in else condition, deactivates dialog box after all dialog has been shown.
+                    if (currentLine >= dialogLines.Length)
+                    {
+                        dialogBox.SetActive(false);
+                    }
+                    else
+                    {
+                        dialogText.text = dialogLines[currentLine];
+                    }
+                }
+                else
+                {
+                    justStarted = false;
+                }
+
+            }
+        }
+    }
+    
+    //function to be called by dialogActivatorscript
+    public void showDialog(string[] newLines)
+    {
+        dialogLines = newLines;
+
+        currentLine = 0;
+
+        dialogText.text = dialogLines[0];
+
+        dialogBox.SetActive(true);
+
+        justStarted = true;
+    }
+}
